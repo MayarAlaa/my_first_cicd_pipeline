b1e837ebaaa126c9e4bbb1a0b3aada1f15bd7f4a
MayarAlaa <mayaralaa12@gmail.com>
2024-10-17 10:09:51 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    return a + b
+
+
+
+
+if __name__ == "__main__":
+    print(f"Addition of 5 and 3 is {add(5,3)}" )
---
+++ test_app.py
@@ -0,0 +1,5 @@
+from app import add
+
+def test_add():
+    assert add(5,3) == 8
+    assert add(2,4) == 6
